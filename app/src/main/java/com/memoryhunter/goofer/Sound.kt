bd9f65f93a56c3fc92ded549b37795982e17cd30
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2023-06-02 19:24:24 +0400

    Implemented playSound

--- app/src/main/java/com/memoryhunter/goofer/Sound.kt
+++ app/src/main/java/com/memoryhunter/goofer/Sound.kt
@@ -1,11 +1,20 @@
 package com.memoryhunter.goofer
 
+import android.content.Context
+import android.media.MediaPlayer
 import android.net.Uri
+import androidx.compose.runtime.MutableState
 
 data class Sound(val name: String, val uri: Uri)
 
 fun playSound(
-    /* TODO */
+    currentMediaPlayer: MutableState<MediaPlayer?>,
+    currentContext: Context,
+    uri: Uri
 ) {
-    /* TODO */
+    currentMediaPlayer.value = MediaPlayer.create(currentContext, uri)
+    currentMediaPlayer.value?.start()
+    currentMediaPlayer.value?.setOnCompletionListener {
+        currentMediaPlayer.value?.release()
+    }
 }
