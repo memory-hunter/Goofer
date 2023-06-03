95de37437d03dca8abce73dbaa49dae1ebc1f871
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2023-06-03 13:28:20 +0400

    Added Divider

--- app/src/main/java/com/memoryhunter/goofer/MainWindow.kt
+++ app/src/main/java/com/memoryhunter/goofer/MainWindow.kt
@@ -4,10 +4,9 @@ import android.media.MediaPlayer
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -16,37 +15,35 @@ import com.memoryhunter.goofer.ui.theme.GooferTheme
 
 @Composable
 fun MainWindow() {
-    val showPopup = remember { mutableStateOf(false) }
-    val soundList = remember { mutableStateListOf<Sound>() }
-
-    val currentMediaPlayer = remember { mutableStateOf<MediaPlayer?>(null) }
     val currentContext = LocalContext.current
+
+    val soundList = remember { mutableStateListOf<Sound>() }
+    val showPopup = remember { mutableStateOf(false) }
+    val currentMediaPlayer = remember { mutableStateOf<MediaPlayer?>(null) }
 
     GooferTheme {
         Column {
             TitleBar(title = stringResource(id = R.string.app_name))
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
             Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.BottomEnd
+                modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomEnd
             ) {
                 SoundboardSection(soundList, currentMediaPlayer, currentContext)
-                AddButton(
-                    onClick = {
-                        showPopup.value = true
-                    }
-                )
+                AddButton(onClick = {
+                    showPopup.value = true
+                })
             }
             if (showPopup.value) {
-                AudioPopup(
-                    onDismissRequest = {
-                        showPopup.value = false
-                    },
-                    onAddAudio = { sound ->
-                        soundList.add(sound)
-                        showPopup.value = false
-                    },
-                    currentMediaPlayer = currentMediaPlayer,
-                    currentContext = currentContext
+                AudioPopup(onDismissRequest = {
+                    showPopup.value = false
+                }, onAddAudio = { sound ->
+                    soundList.add(sound)
+                    showPopup.value = false
+                }, currentMediaPlayer = currentMediaPlayer, currentContext = currentContext
                 )
             }
         }
