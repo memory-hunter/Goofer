e5585a776836f7185f8c1f46337bd47a39e648c1
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2023-06-20 23:39:46 +0400

    Implemented database usage

--- app/src/main/java/com/memoryhunter/goofer/ui/elements/Components.kt
+++ app/src/main/java/com/memoryhunter/goofer/ui/elements/Components.kt
@@ -3,7 +3,6 @@ package com.memoryhunter.goofer.ui.elements
 import android.content.Context
 import android.media.MediaPlayer
 import android.net.Uri
-import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
