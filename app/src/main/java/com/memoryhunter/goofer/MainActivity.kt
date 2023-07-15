85a5963810d9860836db7efcca2cd56e2a9fdcbe
MemoryHunter <59766692+memory-hunter@users.noreply.github.com>
2023-07-16 00:50:04 +0400

    Made decor not visible

--- app/src/main/java/com/memoryhunter/goofer/MainActivity.kt
+++ app/src/main/java/com/memoryhunter/goofer/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import com.memoryhunter.goofer.database.SoundDatabase
 import com.memoryhunter.goofer.database.SoundRepository
 import com.memoryhunter.goofer.database.SoundViewModel
@@ -15,6 +17,13 @@ import com.memoryhunter.goofer.ui.theme.GooferTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v, insets ->
+            v.setPadding(0, 0, 0, 0)
+            insets
+        }
+
         val soundDao = SoundDatabase.getDatabase(applicationContext).soundDao()
         val soundRepository = SoundRepository.getInstance(soundDao)
         val soundViewModel: SoundViewModel by viewModels {
